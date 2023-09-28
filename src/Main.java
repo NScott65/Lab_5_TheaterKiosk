aac1557c9307fab4070be5683e2be7c670f1b82f
Noah <scott.613865@swl.k12.oh.us>
2023-09-28 09:44:49 -0400

    hi

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        //Variables
+        int userAge = 0;
+
+        System.out.print("Enter your age: ");
+        if(scan.hasNextInt()){
+            userAge = scan.nextInt();
+        }else{
+            System.out.print("Your input was invalid.");
+            System.exit(0);
+        }
+        if(userAge < 21){
+            System.exit(0);
+        }else if(userAge >= 21){
+            System.out.print("Now printing your wristband.");
+        }
     }
 }
